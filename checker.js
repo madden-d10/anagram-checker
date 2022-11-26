b6c90c73a120607de12d8c2df7f5a475d86c1c0c
DarraghMadden <darraghmadden7@gmail.com>
2022-11-26 16:04:17 +0000

    uses external storage to add anagrams for usernames

--- checker.js
+++ checker.js
@@ -1,4 +1,4 @@
-cache= {}
+const fs = require('fs');
 
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -13,13 +13,13 @@ function getUsername() {
 
 function getWord1() {
     return new Promise((resolve, reject) => {
-        readline.question(`Word 1: `, answer => resolve(answer))
+        readline.question(`Word 1: `, answer => resolve(answer.trim().toLowerCase()))
     })   
 }
 
 function getWord2() {
     return new Promise((resolve, reject) => {
-        readline.question(`Word 2: `, answer => resolve(answer))
+        readline.question(`Word 2: `, answer => resolve(answer.trim().toLowerCase()))
     })    
 }
 
@@ -29,25 +29,55 @@ function getShouldContinue() {
     })    
 }
 
+function writeDataToFile(data) {
+    fs.writeFile('./externalStorage.json', JSON.stringify(data), err => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('Written to file')
+        });
+}
+
 async function start() {
     let shouldContinue = 'y'
+    const rawData = fs.readFileSync('./externalStorage.json');
+    const data = JSON.parse(rawData);
+    let i = 0
+    let newUserCache = {}
     const username = await getUsername()
+    let userCache = {username, anagrams: {}}
 
     while (shouldContinue === 'y') {
         const word1 = await getWord1()
         const word2 = await getWord2()
-    
-        const anagrams = main(word1, word2)
-        const userCache = {username, anagrams}
-        console.log(userCache)
-        
+
+        // check for user
+        for (entry of data) {
+            // user does not exist
+            if (entry.username !== username) {
+                i++
+                continue
+            } else { // user exists
+                userCache = entry
+                break
+            }
+        }
+
+        newUserCache = main(word1, word2, userCache)
         shouldContinue = await getShouldContinue()
     }
 
+    if (data.length === 0) {
+        data.push(newUserCache)
+    } else {
+        data.splice(i, 1, newUserCache);
+    }
+
+    writeDataToFile(data)
     readline.close();
 }
 
-function main(word1, word2) {
+function main(word1, word2, userCache) {
     const isWord1Valid = checkWordIsValid(word1)
     const isWord2Valid = checkWordIsValid(word2)
 
@@ -56,49 +86,50 @@ function main(word1, word2) {
 
         if (isAnagram) {
             const key = createKey(word1)
-            const wordArray = [word1, word2] 
-    
+            const wordArray = [word1, word2]
+            let { anagrams } = userCache
+
             // check if combination of letters has been used before
-            if (key in cache) {
+            if (key in anagrams) {
                 // check if words exist in the array for that letter combination             
-                const word1Exists = checkForWord(cache[key], word1);
-                const word2Exists = checkForWord(cache[key], word2);
+                const word1Exists = checkForWord(anagrams[key], word1);
+                const word2Exists = checkForWord(anagrams[key], word2);
 
                 // both exist
                 if (word1Exists && word2Exists) {
                     console.log('Both words already have been checked previously')
-                    return cache
+                    return userCache
                 }
                 else if (word1Exists) {
-                    cache[key] = [...cache[key], word2]
+                    console.log(`Added new word: ${word2}`)
+                    anagrams[key] = [...anagrams[key], word2]
                 }
                 else if (word2Exists) {
-                    cache[key] = [...cache[key], word1]
+                    console.log(`Added new word: ${word1}`)
+                    anagrams[key] = [...anagrams[key], word1]
                 }
                 // neither exist
                 else {
-                    cache[key] = [...cache[key], ...wordArray]
+                    console.log(`Added new words: ${word1}, ${word2},`)
+                    anagrams[key] = [...anagrams[key], ...wordArray]
                 }
             } else {
-                cache[key] = [word1, word2]
+                console.log(`Added new words: ${word1}, ${word2},`)
+                anagrams[key] = [word1, word2]
             }
         }
         else {
             console.log('The words are not anagrams')
-            return cache
+            return userCache
         }        
     } else {
         console.log('A word is invalid (No numbers, spaces or special characters')
-        return cache
+        return userCache
     }
-    return cache
+    return userCache
 }
 
 function checkForAnagram(string1, string2) {
-    // trim whitespaces and convert words to lowercase
-    string1 = string1.trim().toLowerCase()
-    string2 = string2.trim().toLowerCase()
-
     // get the character map of both strings
     const charMapA = getCharMap(string1)
     const charMapB = getCharMap(string2)
@@ -128,7 +159,7 @@ function createKey(word) {
 }
 
 function checkWordIsValid(word) {
-    return /^[a-z]+$/.test(word)
+    return /^[a-zA-Z]+$/.test(word)
 }
 
 function checkForWord(wordArray, word) {
